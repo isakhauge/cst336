866cdf58702d061228a780c3964125f455fbe0b5
Isak Hauge <isakhauge@gmail.com>
2019-09-02 19:55:51 -0700

    Insane update.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const rsc = {
+  title: 'Test'
+};
+
+
+/* Landing page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', rsc);
 });
+
+
+/* Portfolio page */
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', {
+    title: 'Portfolio',
+  });
+});
+
+/* Portfolio page */
+router.get('/portfolio/about', function(req, res, next) {
+  res.render('portfolio-about', {
+    title: 'Portfolio',
+  });
+});
+
+/* Portfolio page */
+router.get('/portfolio/contact', function(req, res, next) {
+  res.render('portfolio-contact', {
+    title: 'Portfolio',
+  });
+});
+
 
 module.exports = router;
