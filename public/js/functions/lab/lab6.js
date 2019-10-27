6e373bbb79546e80761b949f1454fd753867b190
Isak Hauge <isakhauge@gmail.com>
2019-10-26 23:12:17 -0700

    Fix performance issue. Use only one Ajax call per solar system object. Get thumbnail from the summary call.

--- public/js/functions/lab/lab6.js
+++ public/js/functions/lab/lab6.js
@@ -1,9 +1,8 @@
 import * as f from '../functions.js';
 
 const QUERY_PARAM = 'view';
-const BASE_URL_IMAGE = 'https://en.wikipedia.org/api/rest_v1/page/media/';
-const BASE_URL_SUMMARY = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
-const ENTITY_QUERIES = {
+const BASE_URL = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
+const SOLAR_SYSTEM_OBJECT_QUERIES = {
 	sun: 'Sun',
 	mercury: 'Mercury_(planet)',
 	venus: 'Venus',
@@ -20,27 +19,17 @@ function getQuery(param) {
 	return urlParams.get(param);
 }
 
-function getImage(query, callback) {
-	f.ajaxFetch('', BASE_URL_IMAGE + query, function(raw) {
-		if (f.isJSON(raw)) {
-			const obj = JSON.parse(raw);
-			f.cout(query + ' image fetched from Wikipedia', 'primary');
-			callback(obj.items[0].thumbnail.source);
-		}
-	});
-}
-
 function getInfo(query, callback) {
-	f.ajaxFetch('', BASE_URL_SUMMARY + query, function(raw) {
+	f.ajaxFetch('', BASE_URL + query, function(raw) {
 		if (f.isJSON(raw)) {
 			const obj = JSON.parse(raw);
-			f.cout(query + ' info fetched from Wikipedia', 'primary');
-			callback(obj.title, obj.extract);
+			f.cout(query + ' fetched from Wikipedia', 'primary');
+			callback(obj.title, obj.thumbnail.source, obj.extract);
 		}
 	});
 }
 
-function createEntityElement(name, imgUrl, summary) {
+function createSolarSystemObject(name, imgUrl, summary) {
 	return `
 		<div class="bg-light p-4 shadow rounded border">
 			<h1 class="mt-0 mb-3">${name}</h1>
@@ -80,13 +69,11 @@ function onLoad() {
 	f.cout('JS Module Loaded', 'success');
 	const query = getQuery(QUERY_PARAM);
 	if (query) {
-		if (Object.values(ENTITY_QUERIES).includes(query)) {
-			getInfo(query, function (title, summary) {
-				getImage(query, function (imgUrl) {
-					insertElement(
-						createEntityElement(title, imgUrl, summary)
-					);
-				});
+		if (Object.values(SOLAR_SYSTEM_OBJECT_QUERIES).includes(query)) {
+			getInfo(query, function (title, imgUrl, summary) {
+				insertElement(
+					createSolarSystemObject(title, imgUrl, summary)
+				);
 			});
 		}
 	} else {
